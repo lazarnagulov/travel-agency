4c39cc5b0b817b2c7f754bd99331238c2275b5fb
Lazar Nagulov <lazarnagulov1@gmail.com>
2023-04-07 15:40:29 +0200

    added main page

---
+++ entities/TravelAgency.js
@@ -0,0 +1,14 @@
+class TravelAgency
+{
+    constructor(name, address, yearOfOpening, logo, phoneNumber, email, destinations)
+    {
+        this.name = name;
+        this.address = address;
+        this.yearOfOpening = yearOfOpening;
+        this.logo = logo;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.destinations = destinations;
+    }
+
+}
---
+++ entities/User.js
@@ -0,0 +1,14 @@
+class User
+{
+    constructor(username, password, name, surname, email, birthDate, address, phoneNumber) 
+    {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
